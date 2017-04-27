64f0545efd3ea8b825e9ad7addb337314f9cd728
Tyler Bailey <Tyler@Tylers-MacBook-Pro.local>
2017-04-27 16:56:33 -0400

    Clean up basic theme

--- inc/base-theme/archive.php
+++ inc/base-theme/archive.php
@@ -11,7 +11,11 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div class="container">
+			<?php while ( have_posts() ) : the_post(); ?>
 
+				<h1><?php the_title(); ?></h1>
+
+			<?php endwhile; // End of the loop. ?>
 		</div><!-- .container -->
 	</div><!-- #primary -->
 
--- inc/base-theme/single.php
+++ inc/base-theme/single.php
@@ -11,12 +11,12 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<div class="container">
-			<?php
-				while ( have_posts() ) : the_post(); ?>
-					<h1><?php the_title(); ?></h1>
-					<?php the_content(); ?>
-				<?php endwhile; // End of the loop.
-			?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				
+				<h1><?php the_title(); ?></h1>
+				<?php the_content(); ?>
+
+			<?php endwhile; // End of the loop. ?>
 		</div><!-- .container -->
 	</div><!-- #primary -->
 <?php
