554db6a05fdfb8cd595fe86936f83a0d0de5b87b
Tyler Bailey <tyler@elexicon.com>
2019-01-16 10:25:51 -0500

    Add removal of WP emoji scripts back. Might add option to disable from dashboard.

--- inc/base-theme-oop/inc/lexi/ThemeInit.php
+++ inc/base-theme-oop/inc/lexi/ThemeInit.php
@@ -126,10 +126,10 @@ class ThemeInit {
 		remove_action('wp_head', 'rest_output_link_wp_head', 10);
 		remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
 		remove_action('template_redirect', 'rest_output_link_header', 11, 0);
-		//remove_action('wp_head', 'print_emoji_detection_script', 7);
-		//remove_action('wp_print_styles', 'print_emoji_styles');
-		//remove_action('admin_print_scripts', 'print_emoji_detection_script');
-		//remove_action('admin_print_styles', 'print_emoji_styles');
+		remove_action('wp_head', 'print_emoji_detection_script', 7);
+		remove_action('wp_print_styles', 'print_emoji_styles');
+		remove_action('admin_print_scripts', 'print_emoji_detection_script');
+		remove_action('admin_print_styles', 'print_emoji_styles');
 	}
 
 	/**
